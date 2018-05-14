b2cd4846f1b064c6b72db0d8c1bbfd4e18b1e9cd
mathieumast <mmast@sword-group.com>
2018-05-14 17:24:44 +0200

    Engine tests

--- engine.go
+++ engine.go
@@ -1,6 +1,9 @@
 package pgrest
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/go-pg/pg"
 	"github.com/go-pg/pg/orm"
 )
@@ -11,8 +14,40 @@ type Engine struct {
 }
 
 // Execute executes a rest query
-func (e *Engine) Execute(db *pg.DB, restQuery *RestQuery) (res orm.Result, err error) {
+func (e *Engine) Execute(db *pg.DB, restQuery *RestQuery) (res interface{}, err error) {
+	if restQuery.Resource == "" {
+		return nil, fmt.Errorf("resource undefined")
+	}
+	resource := e.config.GetResource(restQuery.Resource)
+	if resource == nil {
+		return nil, fmt.Errorf("resource %v not found", restQuery.Resource)
+	}
+	if restQuery.Action|resource.Action == None {
+		return nil, fmt.Errorf("action query %v forbidden: resource action is %v", restQuery.Action, resource.Action)
+	}
+
+	if restQuery.Action == Get {
+		return e.executeActionGet(db, resource, restQuery)
+	}
 	return nil, nil
+}
+
+func (e *Engine) executeActionGet(db *pg.DB, resource *Resource, restQuery *RestQuery) (res interface{}, err error) {
+	if restQuery.Key != "" {
+		element := reflect.New(resource.Type)
+		table := orm.GetTable(resource.Type)
+		if len(table.PKs) == 1 {
+			pk := table.PKs[0]
+			pk.ScanValue(element, []byte(restQuery.Key))
+		} else {
+			return nil, fmt.Errorf("only single pk is permitted for %v", resource.Type)
+		}
+		db.Select(element)
+		return element, nil
+	}
+	elements := reflect.ArrayOf(0, resource.Type)
+	db.Model(elements).Select()
+	return elements, nil
 }
 
 // NewEngine constructs Engine
--- engine_test.go
+++ engine_test.go
@@ -6,20 +6,21 @@ import (
 	"time"
 
 	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
+	"github.com/stretchr/testify/assert"
 )
+
+type Book struct {
+	ID       int64
+	Title    string
+	AuthorID int64
+}
 
 type Author struct {
 	ID        int64
 	Firstname string
 	Lastname  string
-	Books     []*Book // has many relation
-}
-
-type Book struct {
-	ID     int64
-	Title  string
-	Resume string
-	Author Author // has one relation
+	Books     []*Book
 }
 
 func pgOptions() *pg.Options {
@@ -28,6 +29,77 @@ func pgOptions() *pg.Options {
 		Database:           "postgres",
 		IdleCheckFrequency: 100 * time.Millisecond,
 	}
+}
+
+func populate(db *pg.DB, t *testing.T) {
+	for _, model := range []interface{}{(*Author)(nil), (*Book)(nil)} {
+		err := db.CreateTable(model, &orm.CreateTableOptions{
+			Temp: true,
+		})
+		assert.Nil(t, err)
+	}
+	authors := []*Author{{
+		Firstname: "Antoine",
+		Lastname:  "de Saint Exupéry",
+	}, {
+		Firstname: "Franz",
+		Lastname:  "Kafka",
+	}}
+	err := db.Insert(&authors)
+	assert.Nil(t, err)
+	countAuthors, err := db.Model(&Author{}).Count()
+	assert.Nil(t, err)
+	assert.Equal(t, countAuthors, 2)
+
+	books := []*Book{{
+		Title:    "Courrier sud",
+		AuthorID: authors[0].ID,
+	}, {
+		Title:    "Vol de nuit",
+		AuthorID: authors[0].ID,
+	}, {
+		Title:    "Terre des hommes",
+		AuthorID: authors[0].ID,
+	}, {
+		Title:    "Lettre à un otage",
+		AuthorID: authors[0].ID,
+	}, {
+		Title:    "Pilote de guerre",
+		AuthorID: authors[0].ID,
+	}, {
+		Title:    "Le Petit Prince",
+		AuthorID: authors[0].ID,
+	}, {
+		Title:    "La Métamorphose",
+		AuthorID: authors[1].ID,
+	}, {
+		Title:    "La Colonie pénitentiaire",
+		AuthorID: authors[1].ID,
+	}, {
+		Title:    "Le Procès",
+		AuthorID: authors[1].ID,
+	}, {
+		Title:    "Le Château",
+		AuthorID: authors[1].ID,
+	}, {
+		Title:    "L'Amérique",
+		AuthorID: authors[1].ID,
+	}}
+
+	err = db.Insert(&books)
+	assert.Nil(t, err)
+	countBooks, err := db.Model(&Book{}).Count()
+	assert.Nil(t, err)
+	assert.Equal(t, countBooks, 11)
+
+	var selectedAuthors []Author
+	err = db.Model(&selectedAuthors).
+		Relation("Books").
+		Select()
+	assert.Nil(t, err)
+	assert.Equal(t, len(selectedAuthors), 2)
+	assert.Equal(t, len(selectedAuthors[0].Books), 6)
+	assert.Equal(t, len(selectedAuthors[1].Books), 5)
 }
 
 func TestEngine(t *testing.T) {
@@ -39,5 +111,7 @@ func TestEngine(t *testing.T) {
 	db := pg.Connect(pgOptions())
 	defer db.Close()
 
-	engine.Execute(db, &RestQuery{Get, "User", "1", "", 0, 0, nil, nil})
+	populate(db, t)
+
+	engine.Execute(db, &RestQuery{Get, "", "0", "", 0, 0, nil, nil})
 }
